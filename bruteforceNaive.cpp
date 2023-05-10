b988b869e7af4d77f9eca28b8a4e59131af7b20e
Brandon <brandonhernandez_10@outlook.es>
2023-05-10 01:02:04 -0600

    feat: paralel program

--- bruteforceNaive.cpp
+++ bruteforceNaive.cpp
@@ -1,33 +1,62 @@
-//bruteforceNaive.c
-//Tambien cifra un texto cualquiera con un key arbitrario.
-//OJO: asegurarse que la palabra a buscar sea lo suficientemente grande
-//  evitando falsas soluciones ya que sera muy improbable que tal palabra suceda de
-//  forma pseudoaleatoria en el descifrado.
 //>> mpic++ ./bruteforceNaive.cpp -lssl -lcrypto -o desBrute.exe
 //>> mpirun -np 2 desBrute.exe
+//>> mpirun --use-hwthread-cpus --oversubscribe -np 4 ./desBrute.exe
 
-#include <iostream>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
 #include <unistd.h>
-#include <fstream>
 #include <openssl/des.h>
+#include <iostream>
+#include <fstream>
+#include <stdint.h>
+#include <limits.h>
+
+#if SIZE_MAX == UCHAR_MAX
+  #define MPI_SIZE_T MPI_UNSIGNED_CHAR
+#elif SIZE_MAX == USHRT_MAX
+  #define MPI_SIZE_T MPI_UNSIGNED_SHORT
+#elif SIZE_MAX == UINT_MAX
+  #define MPI_SIZE_T MPI_UNSIGNED
+#elif SIZE_MAX == ULONG_MAX
+  #define MPI_SIZE_T MPI_UNSIGNED_LONG
+#elif SIZE_MAX == ULLONG_MAX
+  #define MPI_SIZE_T MPI_UNSIGNED_LONG_LONG
+#else
+  #error "what is happening here?"
+#endif
 
 using namespace std;
 
-#define KEY_SPACE_SIZE 4294967296 // 2^32 possible keys
+//2^56 / 4 es exactamente 18014398509481983
+//long the_key = 18014398509481983L;
+//long the_key = 18014398509481983L +1L;
 
-void long_to_bytes(long input, unsigned char *output) {
+char search[] = "there will be no";
+long the_key = 123456L;
+
+/**
+ * Converts a long number to bytes
+ * @param input the long number that will be converted
+ * @param output the long number transformed to bytes
+*/
+void long_to_bytes(long input, unsigned char *output)
+{
   for (int i = 7; i >= 0; i--) {
     output[i] = input & 0xFF;
     input >>= 8;
   }
 }
 
-//descifra un texto dado una llave
-void decrypt(long mykey, unsigned char *ciph, int len, unsigned char *iv)
+/**
+ * Decrypts a text
+ * @param mykey key that will be used to desencrypt
+ * @param ciph chypered text that will be decrypted (out)
+ * @param len the size of the chyper
+ * @param iv  
+*/
+void decrypt(long mykey, unsigned char* ciph, int len, unsigned char* iv)
 {
 	unsigned char key_bytes[8];
 	long_to_bytes(mykey, key_bytes);
@@ -42,8 +71,13 @@ void decrypt(long mykey, unsigned char *ciph, int len, unsigned char *iv)
 	DES_ncbc_encrypt(ciph, ciph, len, &key_schedule, &ivec, DES_DECRYPT);
 }
 
-//cifra un texto dado una llave
-void encrypt(long mykey, unsigned char *ciph){
+/**
+ * Encrypts a text
+ * @param mykey the key that will be used to encrypt
+ * @param ciph plain text that will be encrypted
+*/
+void encrypt(long mykey, unsigned char *ciph)
+{
   unsigned char key_bytes[8];
   long_to_bytes(mykey, key_bytes);
   DES_cblock key;
@@ -54,70 +88,24 @@ void encrypt(long mykey, unsigned char *ciph){
   DES_ecb_encrypt((const_DES_cblock*)ciph, (const_DES_cblock*)ciph, &key_schedule, DES_ENCRYPT);
 }
 
-//palabra clave a buscar en texto descifrado para determinar si se rompio el codigo
-char search[] = "es una prueba de";
-int tryKey(long initial_guess, unsigned char *ciph, int len, unsigned char *iv) {
-	for (long key_guess = initial_guess; key_guess < KEY_SPACE_SIZE; ++key_guess)
-	{
-		unsigned char *decrypted = (unsigned char *)calloc(len, sizeof(unsigned char));
-		memcpy(decrypted, ciph, len);
-		decrypt(key_guess, decrypted, len, iv);
-
-		// Check if the decrypted message contains the plaintext
-		if (strstr((char *)decrypted, search) != NULL) {
-			memcpy(ciph, decrypted, len);
-			return 1;
-		}
-
-		free(decrypted);
-	}
-	return 0;
-}
-
-void Build_mpi_type(double* a_p, MPI_Datatype* input_mpi_t_p) {
-  MPI_Aint array_of_displacements[3] = {0};
-  MPI_Aint a_addr, b_addr, n_addr;
-
-  MPI_Get_address(a_p, &a_addr);
-
-  array_of_displacements[1] = n_addr-b_addr;
-  array_of_displacements[2] = a_addr-b_addr; 
-
-  int array_of_blocklengths[3] = {1, 1, 1};
-  MPI_Datatype array_of_types[3] = {MPI_DOUBLE, MPI_DOUBLE, MPI_INT};
-  
-  MPI_Type_create_struct(
-    3,
-    array_of_blocklengths, 
-    array_of_displacements,
-    array_of_types,
-    input_mpi_t_p
-  );
-  MPI_Type_commit(input_mpi_t_p);
-  
-}  /* Build_mpi_type */
-
 /**
- * Gets the key that the user will do as input
- * @param my_rank the id of the process
- * @param comm_sz the context in which they are running
- * @param tstart the timer, will start when the user gives the input (out)
- * @param message_len the lenght of the message encrypted (out)
- * @param message the encrypted message (out)
+ * Test the key 
 */
-void Get_input(int my_rank, int comm_sz, double* tstart, size_t* message_len, unsigned char* padded_message) {
-  MPI_Datatype input_mpi_t;
+int tryKey(long key_guess, unsigned char *ciph, int len, unsigned char *iv)
+{
+  unsigned char *decrypted = (unsigned char *)calloc(len, sizeof(unsigned char));
+  memcpy(decrypted, ciph, len);
+  decrypt(key_guess, decrypted, len, iv);
 
-  // Build_mpi_type(a_p, b_p, n_p, &input_mpi_t);
-
-  if (my_rank == 0) {
-    printf("Enter a, b, and n\n");
-    scanf("%lf %lf %d", a_p, b_p, n_p);
-    *tstart = MPI_Wtime();
+  // Check if the decrypted message contains the plaintext
+  if (strstr((char *)decrypted, search) != NULL) {
+    memcpy(ciph, decrypted, len);
+    free(decrypted);
+    return 1;
   }
 
-  MPI_Bcast(b_p, 1, input_mpi_t, 0, MPI_COMM_WORLD);
-  MPI_Type_free(&input_mpi_t);
+  free(decrypted);
+	return 0;
 }
 
 /**
@@ -154,21 +142,22 @@ string getFileBody (string path) {
   return data;
 }
 
-long the_key = 123456L;
-//2^56 / 4 es exactamente 18014398509481983
-//long the_key = 18014398509481983L;
-//long the_key = 18014398509481983L +1L;
 
-int main(int argc, char *argv[]){ //char **argv
+int main(int argc, char *argv[])
+{
   int N, id;
   long upper = (1L << 56); //upper bound DES keys 2^56
   long mylower, myupper;
 	unsigned char iv[8];
-  unsigned char* padded_message;
-  size_t padded_len;
 
-  MPI_Status st;
-  MPI_Request req;
+  double tstart, tend;
+
+  unsigned char* cypher;
+  string fileBody;
+  size_t cypher_len, file_len;
+
+  MPI_Status status;
+  MPI_Request request;
   MPI_Comm comm = MPI_COMM_WORLD;
 
   //INIT MPI
@@ -176,69 +165,73 @@ int main(int argc, char *argv[]){ //char **argv
   MPI_Comm_size(comm, &N);
   MPI_Comm_rank(comm, &id);
 
+  
+  // Sends the content of the file to the other proccesses
+  if (id == 0)
+  {
+    fileBody = getFileBody("./data.txt");
+    file_len = fileBody.size();
+  }
+
+  MPI_Bcast(&file_len, sizeof(size_t), MPI_SIZE_T, 0, MPI_COMM_WORLD);
+
+  unsigned char* message[file_len];
+
+  if (id == 0) strcpy((char*) message, fileBody.data());
+
+  MPI_Bcast(message, sizeof(unsigned char) * file_len, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+
+  // Starts encryption
+  cypher_len = file_len + (8 - file_len % 8);
+  cypher = (unsigned char*)calloc(cypher_len, sizeof(unsigned char));
+  memcpy(cypher, message, file_len);
+  encrypt(the_key, cypher);
+
   long found = 0L;
   int ready = 0;
-
-  // Reads the file and encrypts the message (it will print the encirpted message)
-  if (id == 0) {
-    string fileBody = getFileBody("./data.txt");
-
-    size_t message_len = fileBody.size();
-    unsigned char* message[message_len];
-
-    strcpy((char*) message, fileBody.data());
-
-    padded_len = message_len + (8 - message_len % 8);
-    padded_message = (unsigned char*)calloc(padded_len, sizeof(unsigned char));
-    memcpy(padded_message, message, message_len);
-    encrypt(the_key, padded_message);
-
-    cout << "The encripted message looks like:" << endl;
-    for (size_t i = 0; i < padded_len; ++i) {
-      printf("%02x", padded_message[i]);
-    }
-    cout << endl;
-  }
 
   // Distributes the work
   long range_per_node = upper / N;
   mylower = range_per_node * id;
-  myupper = range_per_node * (id + 1) -1;
+  myupper = range_per_node * (id + 1) - 1;
 
-  if(id == N - 1){
-    myupper = upper;
-  }
-  printf("Process %d lower %ld upper %ld\n", id, mylower, myupper);
+  if (id == N - 1) myupper = upper;
+  printf("Process [%d] lower = %ld upper = %ld\n", id, mylower, myupper);
 
-  // non blocking receive, revisar en el for si alguien ya encontro
-  MPI_Irecv(&found, 1, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &req);
+  // Doesn't block and checks if the someone found the key
+  MPI_Irecv(&found, 1, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &request);
 
-  for(long i = mylower; i<myupper; ++i){
-    MPI_Test(&req, &ready, MPI_STATUS_IGNORE);
-    if (ready)
-      break;  //ya encontraron, salir
+  tstart = MPI_Wtime();
+  for (long i = mylower; i < myupper; ++i)
+  {
+    MPI_Test(&request, &ready, MPI_STATUS_IGNORE);
+    if (ready) break;  // Key found by another proccess
 
-    if (tryKey(i, padded_message, padded_len, iv)) {
+    if (tryKey(i, cypher, cypher_len, iv))
+    {
       found = i;
       printf("Process %d found the key\n", id);
-      for(int node=0; node<N; node++){
-        MPI_Send(&found, 1, MPI_LONG, node, 0, comm); //avisar a otros
+      for(int node = 0; node < N; node++) {
+        MPI_Send(&found, 1, MPI_LONG, node, 0, comm);
       }
       break;
     }
+  }
+  tend = MPI_Wtime();
 
+  // Waints for the rest of the
+  if (id==0)
+  {
+    MPI_Wait(&request, &status);
+    printf("\nTook %f s to run\n", (tend-tstart));
+    decrypt(found, cypher, cypher_len, iv);
+    printf("Key = %li\n\n", found);
+    printf("%s\n", cypher);
   }
 
-  // //wait y luego imprimir el texto
-  // if(id==0){
-  //   MPI_Wait(&req, &st);
-  //   decrypt(found, cipher, ciphlen);
-  //   printf("Key = %li\n\n", found);
-  //   printf("%s\n", cipher);
-  // }
-
+  // FInishing MPI
   printf("Process %d exiting\n", id);
 
-  // FIN entorno MPI
   MPI_Finalize();
+  exit(0);
 }
